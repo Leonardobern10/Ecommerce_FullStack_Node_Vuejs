e2a7f015e88acd1b5599023f4699f9cd74c20345
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-02-24 09:01:28 -0300

    feat: atualização das rotas no frontend

--- catalog-frontend/src/routes/router.js
+++ catalog-frontend/src/routes/router.js
@@ -5,6 +5,7 @@ import Home from '@/views/Home.vue';
 import ProductList from '@/views/ProductList.vue';
 import ProductForm from '@/views/ProductForm.vue';
 import Cart from '@/components/Cart.vue';
+import Orders from '@/views/Orders.vue';
 
 const routes = [
     { path: '/', component: Home },
@@ -14,6 +15,7 @@ const routes = [
     { path: '/api/products/add', component: ProductForm },
     { path: '/api/products/edit/:id', component: ProductForm, props: true },
     { path: '/api/cart', component: Cart },
+    { path: '/api/orders', component: Orders },
 ];
 
 export const router = createRouter({ history: createWebHistory(), routes });
