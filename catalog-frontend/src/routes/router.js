255bf65c0ee4c6049a5f6ab11028af1816b3deca
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-02-23 23:12:12 -0300

    feat: adição de novas rotas para os produtos

--- catalog-frontend/src/routes/router.js
+++ catalog-frontend/src/routes/router.js
@@ -2,11 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue';
 import Home from '@/views/Home.vue';
+import ProductList from '@/views/ProductList.vue';
+import ProductForm from '@/views/ProductForm.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/api/auth/login', component: Login },
     { path: '/api/auth/register', component: Register },
+    { path: '/api/products', component: ProductList },
+    { path: '/api/products/add', component: ProductForm },
+    { path: '/api/products/edit/:id', component: ProductForm, props: true },
 ];
 
 export const router = createRouter({ history: createWebHistory(), routes });
