900b8b3ed7ae7ba31cda4afe339b09dbf4434434
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-02-24 08:35:57 -0300

    fix: correcao de palavra

--- catalog-backend/src/model/Order.js
+++ catalog-backend/src/model/Order.js
@@ -1,11 +1,7 @@
 import mongoose from 'mongoose';
 
 const OrderSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectiId,
-        ref: 'User',
-        required: true,
-    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     items: [
         {
             product: {
