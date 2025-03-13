bb35d05201baa39453a81d6a04d1de10adfa866d
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-03-13 18:31:21 -0300

    feat: Adicionar ordenação por preço na lista de produtos

--- catalog-backend/src/model/Product.js
+++ catalog-backend/src/model/Product.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    brand: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
     imageUrl: { type: String },
+    createdAt: { type: Date, default: Date.now },
 });
 
+productSchema.methods.findSimilarBrands = function (cb) {
+    return mongoose.model('Product').find({ brand: this.brand }, cb);
+};
 export default mongoose.model('Product', productSchema);
