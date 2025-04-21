279bd2204c8c7fbb69a3dac55627b06b425516df
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-04-21 19:59:02 -0300

    fix: ajustando a busca de clientes e pedidos dos clientes

--- catalog-frontend/src/views/Users.vue
+++ catalog-frontend/src/views/Users.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AdminSidebar from '@/components/AdminSidebar.vue';
-import Button from '@/components/Button.vue';
 import { getOrdersById } from '@/services/orderService';
 import getAllUsers from '@/services/userService';
 import { onMounted, reactive, ref } from 'vue';
@@ -9,6 +8,7 @@ const listUsers = ref([]);
 const pedidos = reactive([]);
 
 const getOrders = async (id) => {
+    console.log('Iniciou!');
     pedidos.length = 0; // Limpar pedidos anteriores antes de atribuir novos
     const orders = await getOrdersById(id);
     pedidos.push(...orders); // Usando push(...) para adicionar novos pedidos ao array
@@ -22,7 +22,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="flex flex-row max-md:flex-col h-screen">
+    <div class="flex flex-row max-md:flex-col h-screen w-full">
         <AdminSidebar />
         <div
             class="flex flex-col justify-between items-center h-full overflow-y-auto w-full bg-white/80">
@@ -32,38 +32,59 @@ onMounted(async () => {
                     Aqui se encontram todos os clientes cadastrados em nosso
                     sistema. Para cada cliente é possível consultar seus pedidos
                     pressionando o botão "Pedidos". Caso haja pedidos para esse
-                    cliente, ao fim da página serão mostrados.
+                    cliente, à direita eles serão mostrados.
                 </p>
             </div>
             <div
-                class="grid grid-flow-row grid-cols-2 max-lg:grid-cols-1 gap-4 w-6/8 max-md:w-full rounded-container p-5">
-                <li
-                    class="list-none my-4 h-fit bg-black/50 rounded-container border-black p-5 text-lg max-md:text-sm text-gray-200/80"
-                    v-for="item in listUsers"
-                    :key="item._id">
-                    <!-- estava errado antes -->
-                    <p><strong>ID:</strong> {{ item._id }}</p>
-                    <p><strong>User:</strong> {{ item.name }}</p>
-                    <p><strong>E-mail: </strong>{{ item.email }}</p>
-                    <p class="mb-4">
-                        <strong>Role: </strong
-                        >{{ String(item.role).toUpperCase() }}
-                    </p>
-                    <Button
-                        ref="#pedidos"
-                        button-name="Pedidos"
-                        @click="getOrders(item._id)" />
-                </li>
+                class="flex flex-row justify-between max-lg:grid-cols-1 gap-2 w-full rounded-container p-5">
+                <table class="w-1/2 table-auto border-collapse">
+                    <caption class="font-bold text-lg">
+                        Tabela com todos os usuários cadastrados
+                    </caption>
+                    <thead>
+                        <tr>
+                            <th class="table-head">ID</th>
+                            <th class="table-head">Username</th>
+                            <th class="table-head">E-mail</th>
+                            <th class="table-head">Nivel de acesso</th>
+                            <th class="table-head">Buscar pedidos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="user in listUsers" :key="user._id">
+                            <td class="border-2 p-0.5 text-center">
+                                {{ user._id }}
+                            </td>
+                            <td class="border-2 p-0.5 text-center">
+                                {{ user.name }}
+                            </td>
+                            <td class="border-2 p-0.5 text-center">
+                                {{ user.email }}
+                            </td>
+                            <td class="border-2 p-0.5 text-center">
+                                {{ user.role }}
+                            </td>
+                            <td class="border-2 p-0.5 text-center">
+                                <button
+                                    @click="getOrders(user._id)"
+                                    class="p-1 text-xs border-2 rounded-container bg-xanadu/50">
+                                    Pesquisar
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
                 <div
                     v-if="pedidos.length !== 0"
-                    class="h-full col-start-0"
-                    id="pedidos ">
+                    class="h-full col-start-0 gap-2"
+                    id="pedidos">
+                    <h2 class="font-bold text-lg">Pedidos buscados abaixo:</h2>
                     <li
-                        class="list-none text-black text-sm m-4 p-4 bg-black/30 rounded-container"
+                        class="list-none text-black text-xs p-4 bg-black/30 mb-1"
                         v-for="pedido in pedidos"
                         :key="pedido.id">
                         <h3
-                            class="flex flex-col items-center font-bold text-xl mb-4">
+                            class="flex flex-col items-center font-bold text-sm mb-4">
                             Nº Pedido: <span>{{ pedido._id }}</span>
                         </h3>
                         <div>
@@ -78,28 +99,40 @@ onMounted(async () => {
                             <strong>{{ pedido.status }}</strong>
                         </div>
                         <div
-                            class="flex flex-col gap-4 items-center justify-between text-gray-200/50 p-4 mt-5 rounded-container">
-                            <p class="text-lg font-bold">Itens do pedido:</p>
-                            <div
-                                class="flex flex-col items-center justify-between w-full text-sm gap-4 p-3 h-fit"
-                                v-for="item in pedido.items"
-                                :key="item.product">
-                                <div class="w-1/2">
-                                    <img
-                                        :src="item.imageUrl"
-                                        alt=""
-                                        class="bg-white rounded-container" />
-                                </div>
-                                <div
-                                    class="flex flex-col justify-between items-center h-full">
-                                    <p class="flex flex-col items-center">
-                                        ID produto:
-                                        <span>{{ item.product }}</span>
-                                    </p>
-                                    <p>Quantidade: {{ item.quantity }}</p>
-                                    <p>Valor: R$ {{ item.price }}</p>
-                                </div>
-                            </div>
+                            class="flex flex-col gap-4 items-center justify-between mt-5">
+                            <table class="w-full">
+                                <caption class="font-bold text-lg">
+                                    Itens do pedido
+                                </caption>
+                                <thead>
+                                    <tr>
+                                        <th class="p-1 border-2 text-center">
+                                            Produto
+                                        </th>
+                                        <th class="p-1 border-2 text-center">
+                                            Quantidade
+                                        </th>
+                                        <th class="p-1 border-2 text-center">
+                                            Valor
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr
+                                        v-for="item in pedido.items"
+                                        :key="item.product">
+                                        <td class="p-1 border-2 text-center">
+                                            {{ item._id }}
+                                        </td>
+                                        <td class="p-1 border-2 text-center">
+                                            {{ item.quantity }}
+                                        </td>
+                                        <td class="p-1 border-2 text-center">
+                                            {{ item.price }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </li>
                 </div>
